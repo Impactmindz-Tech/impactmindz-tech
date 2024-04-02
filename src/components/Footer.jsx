9f700eff94e98f8bd80da87b55e8cd6be6b92c1f
Impactmindz-Tech <impactmindztechsolutions@gmail.com>
2024-04-02 16:10:28 +0530

    minur issue

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -23,57 +23,59 @@ const Footer = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className='flex md:flex-wrap text-white gap-9'>
-                            <div className='w-1/4 md:w-full'>
-                                <img className='w-20 h-16' src={logo} alt="" />
-                                <p className='text-white text-base pt-5'>Revest is a platform offering anyone the ability to invest and potentially earn money from property at the click of a button</p>
+                        <div className=' text-white gap-9'>
+                            <div className='text-center'>
+                                <figure className='flex justify-center'>
+                                    <img className='w-20 h-16' src={logo} alt="" />
+                                </figure>
+                                <p className='text-white text-base pt-5 px-64 md:px-0 md:pb-3'>Revest is a platform offering anyone the ability to invest and potentially earn money from property at the click of a button</p>
                             </div>
-                            {
-                                footerLinks.map((item, i) => (
-                                    <div key={i} className='flex md:flex-wrap w-[75%] gap-9 md:w-full'>
-                                        <div className='w-1/4 md:w-[42%]'>
-                                            <h6 className='text-lg font-semibold'>Company</h6>
-                                            {item.company.map((compantLinks, i) => (
-                                                <NavLink key={i}>
-                                                    <div  className='border-b  border-[#3a4153]'>
-                                                        <p  className='text-white text-base py-2 mt-3 hover:translate-x-3 transition-all'>{compantLinks.name}</p>
-                                                    </div>
-                                                </NavLink>
-                                            ))}
+                            {/* {
+                                    footerLinks.map((item, i) => (
+                                        <div key={i} className='flex md:flex-wrap w-[75%] gap-9 md:w-full'>
+                                            <div className='w-1/4 md:w-[42%]'>
+                                                <h6 className='text-lg font-semibold'>Company</h6>
+                                                {item.company.map((compantLinks, i) => (
+                                                    <NavLink key={i}>
+                                                        <div  className='border-b  border-[#3a4153]'>
+                                                            <p  className='text-white text-base py-2 mt-3 hover:translate-x-3 transition-all'>{compantLinks.name}</p>
+                                                        </div>
+                                                    </NavLink>
+                                                ))}
+                                            </div>
+                                            <div className='w-1/4 md:w-[42%]'>
+                                                <h6 className='text-lg font-semibold'>Invest</h6>
+                                                {item.invest.map((invest, i) => (
+                                                    <NavLink key={i}>
+                                                        <div className='border-b  border-[#3a4153]'>
+                                                            <p  className='text-white text-base py-2 mt-3 hover:translate-x-3 transition-all'>{invest.name}</p>
+                                                        </div>
+                                                    </NavLink>
+                                                ))}
+                                            </div>
+                                            <div className='w-1/4 md:w-[42%]'>
+                                                <h6 className='text-lg font-semibold'>Insights</h6>
+                                                {item.Insights.map((Insights, i) => (
+                                                    <NavLink key={i}>
+                                                        <div className='border-b  border-[#3a4153]'>
+                                                            <p className='text-white text-base py-2 mt-3 hover:translate-x-3 transition-all'>{Insights.name}</p>
+                                                        </div>
+                                                    </NavLink>
+                                                ))}
+                                            </div>
+                                            <div className='w-1/4 md:w-[42%]'>
+                                                <h6 className='text-lg font-semibold'>Legal</h6>
+                                                {item.legal.map((legal, i) => (
+                                                    <NavLink key={i}>
+                                                        <div className='border-b  border-[#3a4153]'>
+                                                            <p  className='text-white text-base py-2 mt-3 hover:translate-x-3 transition-all'>{legal.name}</p>
+                                                        </div>
+                                                    </NavLink>
+                                                ))}
+                                            </div>
                                         </div>
-                                        <div className='w-1/4 md:w-[42%]'>
-                                            <h6 className='text-lg font-semibold'>Invest</h6>
-                                            {item.invest.map((invest, i) => (
-                                                <NavLink key={i}>
-                                                    <div className='border-b  border-[#3a4153]'>
-                                                        <p  className='text-white text-base py-2 mt-3 hover:translate-x-3 transition-all'>{invest.name}</p>
-                                                    </div>
-                                                </NavLink>
-                                            ))}
-                                        </div>
-                                        <div className='w-1/4 md:w-[42%]'>
-                                            <h6 className='text-lg font-semibold'>Insights</h6>
-                                            {item.Insights.map((Insights, i) => (
-                                                <NavLink key={i}>
-                                                    <div className='border-b  border-[#3a4153]'>
-                                                        <p className='text-white text-base py-2 mt-3 hover:translate-x-3 transition-all'>{Insights.name}</p>
-                                                    </div>
-                                                </NavLink>
-                                            ))}
-                                        </div>
-                                        <div className='w-1/4 md:w-[42%]'>
-                                            <h6 className='text-lg font-semibold'>Legal</h6>
-                                            {item.legal.map((legal, i) => (
-                                                <NavLink key={i}>
-                                                    <div className='border-b  border-[#3a4153]'>
-                                                        <p  className='text-white text-base py-2 mt-3 hover:translate-x-3 transition-all'>{legal.name}</p>
-                                                    </div>
-                                                </NavLink>
-                                            ))}
-                                        </div>
-                                    </div>
-                                ))
-                            }
+                                    ))
+                                } */}
                         </div>
                     </div>
                 </Container>

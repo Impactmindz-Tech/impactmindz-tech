8661e83421a1fcfd421d4b5dc6a90f56ca7777cc
Impactmindz-Tech <impactmindztechsolutions@gmail.com>
2024-04-02 15:55:13 +0530

    minur issue

--- src/components/Slider-site.jsx
+++ src/components/Slider-site.jsx
@@ -13,6 +13,31 @@ const SliderSite = ({ sliderData }) => {
         slidesToShow: 4,
         slidesToScroll: 1,
         autoplay: true,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                initialSlide: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
     };
 
     return (
